bc3b8b7c2439abb9cc3430a47438cea24a73409b
rezamnf <rezamnf05@gmail.com>
2021-11-22 19:21:26 +0700

    upload file tugas container

---
+++ php/src/welcome.php
@@ -0,0 +1,17 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: index.php");
+    exit;
+}
+
+?>
+
+<html>
+<body>
+<a href="logout.php">Logout now</a>
+</body>
+</html>
